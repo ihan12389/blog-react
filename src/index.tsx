d262c78f583a01ac02eb6f8099679efcbc357dcc
ihan12389 <ihan1289@gmail.com>
2021-09-10 22:40:09 +0900

    Setting Global Style

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import GlobalStyle from "./style/GlobalStyle";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.Fragment>
+    <GlobalStyle />
+    <App />
+  </React.Fragment>,
+  document.getElementById("root")
+);
