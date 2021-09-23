6b5536defb1fb06e4bff1d8886ee412097a6d310
ihan12389 <ihan1289@gmail.com>
2021-09-23 12:43:06 +0900

    Add Redux Persist

--- src/index.tsx
+++ src/index.tsx
@@ -10,22 +10,41 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "./reducers";
 import rootSaga from "./sagas";
+/* Persist를 위한 모듈들을 로드 */
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+// Make Persist Config
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
 // Saga Middleware 생성
 const sagaMiddleware = createSagaMiddleware();
 
+// Make enhancedReducer
+const enhancedReducer = persistReducer(persistConfig, rootReducer);
+
 // Store 생성 후 Reducer와 Middleware 적용
 const store = createStore(
-  rootReducer,
+  enhancedReducer,
   composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
+
+// Create Persistor
+const persistor = persistStore(store);
 
 // Middleware 실행
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
